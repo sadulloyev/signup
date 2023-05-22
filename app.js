31ac39399c276d93552bf0f0e820222bd3289502
shukhrat <sadullayevsh24@gmail.com>
2023-05-22 11:50:13 +0500

    sing-up

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+    const email = req.body.Email;
+    const password1 = req.body.Lpassword;
+    const firstName = req.body.Fname;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: "firstName",
+                    LNAME: "lastName",
+
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "";
+    const request = https.request(url, options, function (response) {
+        if (request.statusCode === 200) {
+            res.send("OK");
+        } else {
+            res.send("Error");
+        }
+        respense.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(3000, () => {
+    console.log("listening  on port 3000");   //
+});
